3663b93b83f11a068ccb712afc68ef20e45b9a8a
gurb <pcgezer@hotmail.com>
2023-11-20 23:05:33 +0300

    mvvm

--- mgeditor/App.axaml.cs
+++ mgeditor/App.axaml.cs
@@ -1,13 +1,29 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using mgeditor.services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Windows;
 
 namespace mgeditor
 {
     public partial class App : Application
     {
+        public IServiceProvider? ServiceProvider { get; private set; }
+        public IConfiguration? Configuration { get; private set; }
+
         public override void Initialize()
         {
+            var builder = new ConfigurationBuilder();
+
+            Configuration = builder.Build();
+
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            ServiceProvider = serviceCollection.BuildServiceProvider();
             AvaloniaXamlLoader.Load(this);
         }
 
@@ -19,6 +35,11 @@ namespace mgeditor
             }
 
             base.OnFrameworkInitializationCompleted();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(typeof(TabManager));
         }
     }
 }
---
+++ mgeditor/models/File.cs
@@ -0,0 +1,18 @@
+﻿using mgeditor.models.enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mgeditor.models
+{
+    public class File
+    {
+        public FileType FileType { get; set; }
+        public string? Name { get; set; }
+        public Folder? ParentFolder { get; set; }
+        public string? Data { get; set; }
+        public string? TabGuid { get; set; }
+    }
+}
