0674d359835411b5699c3904c65eeecff8e10ea9
gurb <pcgezer@hotmail.com>
2023-11-25 21:56:06 +0300

    di

--- mgeditor/App.axaml.cs
+++ mgeditor/App.axaml.cs
@@ -2,8 +2,10 @@ using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using mgeditor.services;
+using mgeditor.viewmodels;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System;
 using System.Windows;
 
@@ -11,19 +13,40 @@ namespace mgeditor
 {
     public partial class App : Application
     {
-        public IServiceProvider? ServiceProvider { get; private set; }
-        public IConfiguration? Configuration { get; private set; }
+        public IHost AppHost
+        {
+            get;
+        }
+
+        
+        public static T GetService<T>()
+            where T : class
+        {
+            if ((App.Current as App)!.AppHost.Services.GetService(typeof(T)) is not T service)
+            {
+                throw new ArgumentException($"{typeof(T)} not founded in ConfigureServices");
+            }
+
+            return service;
+        }
+
+        public App()
+        {
+            
+            AppHost = Host.CreateDefaultBuilder()
+                .UseContentRoot(AppContext.BaseDirectory)
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddTransient<MainViewModel>();
+                    services.AddTransient<ProjectTreeViewModel>();
+                })
+                .Build();
+
+        }
 
         public override void Initialize()
         {
-            var builder = new ConfigurationBuilder();
 
-            Configuration = builder.Build();
-
-            var serviceCollection = new ServiceCollection();
-            ConfigureServices(serviceCollection);
-
-            ServiceProvider = serviceCollection.BuildServiceProvider();
             AvaloniaXamlLoader.Load(this);
         }
 
@@ -35,11 +58,6 @@ namespace mgeditor
             }
 
             base.OnFrameworkInitializationCompleted();
-        }
-
-        private void ConfigureServices(IServiceCollection services)
-        {
-            services.AddSingleton(typeof(TabManager));
         }
     }
 }
--- mgeditor/views/MainView.axaml.cs
+++ mgeditor/views/MainView.axaml.cs
@@ -1,15 +1,19 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using mgeditor.viewmodels;
 
 namespace mgeditor;
 
 public partial class MainView : UserControl
 {
+
+    public MainViewModel ViewModel { get; private set; }
+
     public MainView()
     {
+        ViewModel = App.GetService<MainViewModel>();
+        DataContext = ViewModel;
         InitializeComponent();
     }
-
-
 }
